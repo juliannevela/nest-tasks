2871eec5ca65e23e112e17e109a5981745cb262f
Juli Vela <hello@juliannevela.dev>
2023-02-13 15:54:48 -0800

    Add filter/search to getTasks route

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 
 @Injectable()
@@ -9,6 +10,25 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let currentTasks = this.getAllTasks();
+
+    if (status) {
+      currentTasks = this.tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      currentTasks = this.tasks.filter(
+        ({ title, description }) =>
+          title.includes(search) || description.includes(search),
+      );
+    }
+
+    return currentTasks;
   }
 
   getTaskById(id: string): Task {
